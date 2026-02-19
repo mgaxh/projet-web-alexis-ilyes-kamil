4f4031362b00fb79ff471445daf0c5895412129b
Alexis <bourquardalexis64@gmail.com>
2026-02-19 14:48:40 +0100

    Page A propos, update formations et css

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 function developper(button) {
-    const parent = button.parentElement; // le <li>
+    const parent = button.parentElement;
     const list = parent.querySelector('.underlist');
     if (list.style.display === "block") {
         list.style.display = "none";
     } else {
         list.style.display = "block";
+    }
+}
+
+function Afficher_click(bouton) {
+    const contenu = bouton.nextElementSibling;
+
+    if (contenu) {
+        if (contenu.style.display === "block") {
+            contenu.style.display = "none";
+        } else {
+            contenu.style.display = "block";
+        }
     }
 }
